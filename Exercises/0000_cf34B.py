f3afa8a714adf6a42163ecdc8454ba3983e3c53b
SqYang-git <727572030@qq.com>
2024-10-16 13:39:23 +0800

    Delete Exercises/0000_cf34B.py

--- Exercises/0000_cf34B.py
+++
@@ -1,9 +0,0 @@
-n, m = map(int, input().split())
-lista = list(map(int, input().split()))
-lista.sort()
-earn = 0
-for i in range(m):
-    if lista[i] > 0:
-        break
-    earn += -lista[i]
-print(earn)
