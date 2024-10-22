002077a9ddbb6010c39d10ebf4bc3564ada644c4
SqYang-git <727572030@qq.com>
2024-10-22 17:26:40 +0800

    Update 1009_oj04148.py

--- Exercises/1009_oj04148.py
+++ Exercises/1009_oj04148.py
@@ -3,9 +3,6 @@ while True:
     p, e, i, d = map(int, input().split())
     if [p, e, i, d] == [-1, -1, -1, -1]:
         break
-    p %= 23
-    e %= 28
-    i %= 33
 
     count = d+1
     while (count-p) % 23 != 0 or (count-e) % 28 != 0 or (count-i) % 33 != 0:
